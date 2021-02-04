fd8e4bf73e4903d7f245f081b2d5e10d85a12f3c
sshinn0224 <sshinn0224@gmail.com>
2021-02-04 12:50:48 +0900

    join process ing...

--- app/src/main/java/com/example/mytoy/join/JoinActivity.java
+++ app/src/main/java/com/example/mytoy/join/JoinActivity.java
@@ -1,7 +1,9 @@
 package com.example.mytoy.join;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
@@ -31,4 +33,27 @@ public class JoinActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    public void btnAuthChk(View view) {
+        // 인증 버튼 전송
+
+        // 안내문구 dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("확인");
+        builder.setMessage("인증 번호를 전송 하였습니다.");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+
+    }
+
+
 }
